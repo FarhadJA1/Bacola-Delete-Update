91edf9c687e76953bcef422dfc91b71043710fe4
Farhad Azadaliyev <farhadja@code.edu.az>
2022-03-29 01:36:32 +0400

    initial

---
+++ Bacola-MVC(Updated)/Areas/AdminArea/Controllers/SliderController.cs
@@ -0,0 +1,26 @@
+﻿using Bacola_MVC_Updated_.Data;
+using Bacola_MVC_Updated_.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bacola_MVC_Updated_.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> slider = await _context.Sliders.ToListAsync();
+            return View(slider);
+        }
+    }
+}
